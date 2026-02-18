88935c7eaa4213e3cfdf23bc1da55e65796f3580
DominicDev <s0l0estudi4r@gmail.com>
2026-02-18 18:06:46 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'OUTKAST';
+export let nameSongs = [
+	"MS. JACKSON", /*1*/
+	"SO FRESH, SO CLEAN", /*2*/
+	"B.O.B. (BOMBS OVER BAGHDAD)", /*3*/
+	"ROSES", /*4*/
+	"HEY YA!", /*5*/
+	"THE WAY YOU MOVE", /*6*/
+	"ROSA PARKS", /*7*/
+	"SPOTTIEOTTIEDOPALISCIOUS", /*8*/
+	"AQUEMINI", /*9*/
+	"SYNTHESIZER", /*10*/
+	"RETURN OF THE “G”", /*11*/
+	"LIBERATION", /*12*/
+	"CHONKYFIRE", /*13*/
+	"MAMACITA", /*14*/
+	"SLUMP", /*15*/
+	"WEST SAVANNAH", /*16*/
+	"DA ART OF STORYTELLIN' (PART 1)", /*17*/
+	"DA ART OF STORYTELLIN' (PART 2)", /*18*/
+	"THE WHOLE WORLD", /*19*/
+	"BABYLON", /*20*/
+]
